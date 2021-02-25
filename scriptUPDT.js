626588fb28888f1875eb1aa875450d2e8626626d
Herman Sabo <szabo.herman@gmail.com>
2021-02-26 00:27:52 +0400

    #2 style and js update.

---
+++ scriptUPDT.js
@@ -0,0 +1,13 @@
+// Basic set of hand variants in this game
+const handVariants = ['rock', 'paper', 'scissors'];
+
+// after user clickes 'PLAY!' button, it dissapeares and two inline divs appear
+// one div is user input div - 3 buttons
+// another div to display machine choise - acter user pickes hand, machine generates 
+//random hand variant
+
+//below those divs there is a score div
+
+//when one of players win, inline divs dissapear and "play again" & final score displayed
+
+//after player hits "play again", score is reset, inline divs appear and game goes on
