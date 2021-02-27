7aa419acf543bf18dcc8a5be858cc75f7a9675ae
Herman Sabo <szabo.herman@gmail.com>
2021-02-27 17:50:19 +0400

    #2 finished game init button and moved to playing stage.

--- scriptUPDT.js
+++ scriptUPDT.js
@@ -1,6 +1,57 @@
 // Basic set of hand variants in this game
 const handVariants = ['rock', 'paper', 'scissors'];
+let initBtnTxt = 'play!';
+const playArea = document.querySelector('#playArea');
 
+
+// game init button
+const playBtn = document.createElement('button');
+playBtn.classList.add('glow-on-hover', 'play-btn');
+playBtn.setAttribute('type', 'button');
+playBtn.textContent = initBtnTxt;
+
+// game init button parent div
+const initDiv = document.createElement('div');
+initDiv.classList.add('gameInit', 'd-flex', 'justify-content-center', 'align-items-center');
+initDiv.appendChild(playBtn);
+
+// create div to contain player side of hands
+const playerSide = document.createElement('div');
+playerSide.classList.add('playerSide', 'col-lg-6');
+playerSide.innerText = 'test playerSide';
+
+// create div to contain machine side of hand
+const machineSide = document.createElement('div');
+machineSide.classList.add('machineSide', 'col-lg-6');
+machineSide.innerText = 'test machineSide';
+
+// create score board on bottom of player-machine divs
+const score = document.createElement('div');
+score.classList.add('score');
+score.innerText = 'test score';
+
+// create row to wrap player and machine sides 
+const handsWrapper = document.createElement('div');
+handsWrapper.classList.add('row', 'd-flex', 'justify-content-center', 'align-items-center', 'handsWrapper');
+handsWrapper.appendChild(playerSide);
+handsWrapper.appendChild(machineSide);
+
+
+
+
+
+
+function game() {
+    playArea.appendChild(initDiv);
+    playBtn.addEventListener('click', () => {
+        playArea.removeChild(initDiv);
+        playArea.appendChild(handsWrapper);
+        playArea.appendChild(score);
+
+    })
+}
+
+game();
 // after user clickes 'PLAY!' button, it dissapeares and two inline divs appear
 // one div is user input div - 3 buttons
 // another div to display machine choise - acter user pickes hand, machine generates 
@@ -11,3 +62,4 @@ const handVariants = ['rock', 'paper', 'scissors'];
 //when one of players win, inline divs dissapear and "play again" & final score displayed
 
 //after player hits "play again", score is reset, inline divs appear and game goes on
+
